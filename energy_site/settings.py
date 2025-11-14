37b1437501822bcb70d59bad7a8416d941a03951
Анастасія <anastasia@MAC-01A45B.station>
2025-11-14 18:03:18 +0000

    Add STATIC_ROOT

--- energy_site/settings.py
+++ energy_site/settings.py
@@ -125,6 +125,7 @@ import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
