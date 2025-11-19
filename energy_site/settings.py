2b877da9c8b32aa6efa67bd218818f0404892d96
Анастасія <anastasia@MAC-01A45B.station>
2025-11-19 00:36:47 +0000

    Fix Cloudinary storage config

--- energy_site/settings.py
+++ energy_site/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # ----------------------
 # MEDIA — CLOUDINARY
 # ----------------------
