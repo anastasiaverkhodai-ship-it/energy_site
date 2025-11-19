963bbd5a0d5ccdd02c9d4a8b47f2237445e195e5
Анастасія <anastasia@mac.nuigalway.ie>
2025-11-19 12:29:19 +0000

    Fix Uploadcare settings

--- main/admin.py
+++ main/admin.py
@@ -2,8 +2,41 @@
 
 # Register your models here.
 from django.contrib import admin
-from .models import TariffDocument
+from django import forms
+from pyuploadcare.dj.forms import FileWidget
+
+from .models import Document, TariffDocument
+
+
+# ----- Document -----
+
+class DocumentAdminForm(forms.ModelForm):
+    class Meta:
+        model = Document
+        fields = "__all__"
+        widgets = {
+            "file": FileWidget,     # Uploadcare widget
+        }
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    form = DocumentAdminForm
+    list_display = ("title", "file")
+
+
+# ----- TariffDocument -----
+
+class TariffDocumentAdminForm(forms.ModelForm):
+    class Meta:
+        model = TariffDocument
+        fields = "__all__"
+        widgets = {
+            "file": FileWidget,     # Uploadcare widget
+        }
+
 
 @admin.register(TariffDocument)
 class TariffDocumentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'year', 'file')
+    form = TariffDocumentAdminForm
+    list_display = ("name", "year", "file")
