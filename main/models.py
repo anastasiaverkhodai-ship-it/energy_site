c3cd0ea65f0ffa8b398be42e0e2380cbe1466f76
Анастасія <anastasia@mac.nuigalway.ie>
2025-11-19 12:23:48 +0000

    Fix Uploadcare settings

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from pyuploadcare.dj.models import FileField   # важливо!
 
 class Document(models.Model):
     title = models.CharField(max_length=255)
-    file = FileField()   # Uploadcare зберігає файл у хмарі
+    file = FileField(blank=True, null=True)   # Uploadcare зберігає файл у хмарі
 
     def __str__(self):
         return self.title
@@ -12,7 +12,7 @@ class Document(models.Model):
 class TariffDocument(models.Model):
     year = models.IntegerField()
     name = models.CharField(max_length=255)
-    file = FileField()   # також через Uploadcare
+    file = FileField(blank=True, null=True)   # також через Uploadcare
 
     def __str__(self):
         return f"{self.name} ({self.year})"
